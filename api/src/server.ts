38b2867085d3332dd736098c2edc9748db832af5
Daniel <danielsaraldi@gmail.com>
2021-02-22 21:58:55 -0300

    feat: add routes

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,4 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (request, response) => response.json({ message: "Hello World!" }));
+
+app.post("/", (request, response) =>
+  response.json({ message: "Os dados foram salvos com sucesso!" })
+);
+
 app.listen(3333, () => console.log("Server is running!"));
