7e0488d2f66b5632b49c48e03f480945023dfb69
Daniel <danielsaraldi@gmail.com>
2021-02-25 20:49:22 -0300

    test: ensure that be able to create a new user

---
+++ api/src/__tests__/User.test.ts
@@ -0,0 +1,21 @@
+import request from "supertest";
+import { app } from "../app";
+
+import createConnection from "../database";
+
+describe("Users", async () => {
+  // Before all is create database connection and given run migrations
+  beforeAll(async () => {
+    const connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  it("Should be able to create a new user", async () => {
+    const response = await request(app).post("/users").send({
+      name: "User Example",
+      email: "user@example.com",
+    });
+
+    expect(response.status).toBe(201);
+  });
+});
