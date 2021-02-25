6008b8845238632b936c9ac9e87f26762cdd0740
Daniel <danielsaraldi@gmail.com>
2021-02-25 20:49:02 -0300

    ensure that be able to create a new user

--- api/src/database/index.ts
+++ api/src/database/index.ts
@@ -1,3 +1,17 @@
-import { createConnection } from "typeorm";
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-createConnection();
+// Defines the type of bank to connect
+export default async (): Promise<Connection> => {
+  // Get datas to ormconfig.json
+  const defaultOptions = await getConnectionOptions();
+
+  return createConnection(
+    // Overwrite the object with ormconfig.json data from the database
+    Object.assign(defaultOptions, {
+      database:
+        process.env.NODE_ENV === "test"
+          ? "./src/database/database.test.sqlite"
+          : defaultOptions.database,
+    })
+  );
+};
