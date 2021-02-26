a57eaae9e91ce056a09d2ecd76fa5a1b82c8fcbe
Daniel <danielsaraldi@gmail.com>
2021-02-25 21:05:54 -0300

    ci: add posttest to remove test database when run jest

--- api/src/__tests__/User.test.ts
+++ api/src/__tests__/User.test.ts
@@ -19,7 +19,7 @@ describe("Users", () => {
     expect(response.status).toBe(201);
   });
 
-  it("Should not be able to create a user with exists email", async () => {
+  it("Should not be able to create an user with exists email", async () => {
     const response = await request(app).post("/users").send({
       name: "User Example",
       email: "user@example.com",
