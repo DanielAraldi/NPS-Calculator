4f625ce509ab32ca62477221503de0dc4c9b90e6
Daniel <danielsaraldi@gmail.com>
2021-02-25 21:22:16 -0300

    test: ensure that must get all surveys

--- api/src/__tests__/Survey.test.ts
+++ api/src/__tests__/Survey.test.ts
@@ -16,6 +16,17 @@ describe("Surveys", () => {
     });
 
     expect(response.status).toBe(201);
-    expect(response.body).toHaveProperty("id"); 
+    expect(response.body).toHaveProperty("id");
+  });
+
+  it("Should be able to get all surveys", async () => {
+    await request(app).post("/surveys").send({
+      title: "Title Example #2",
+      description: "Description Example #2",
+    });
+
+    const response = await request(app).get("/surveys");
+
+    expect(response.body.length).toBe(2);
   });
 });
