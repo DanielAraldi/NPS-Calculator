60ebb77c5429fd4634a706168cc06bb658b84b0d
Daniel <danielsaraldi@gmail.com>
2021-02-26 21:33:51 -0300

    feat: seed email done

--- api/src/controllers/SendEmailController.ts
+++ api/src/controllers/SendEmailController.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from "typeorm";
 import { SurveysRepository } from "../repositories/SurveysRepository";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 import { UsersRepository } from "../repositories/UsersRepository";
+import SendEmailService from "../services/SendEmailService";
 
 class SendEmailController {
   async execute(request: Request, response: Response) {
@@ -19,11 +20,11 @@ class SendEmailController {
     if (!userAlreadyExists)
       return response.status(400).json({ error: "User does not exists!" });
 
-    const surveyAlreadyExists = await surveysRepository.findOne({
+    const survey = await surveysRepository.findOne({
       id: survey_id, // that's equals the id === survey_id
     });
 
-    if (!surveyAlreadyExists)
+    if (!survey)
       return response.status(400).json({ error: "Survey does not exists!" });
 
     const surveyUser = surveysUsersRepository.create({
@@ -31,6 +32,8 @@ class SendEmailController {
       survey_id,
     });
     await surveysUsersRepository.save(surveyUser);
+
+    await SendEmailService.execute(email, survey.title, survey.description)
 
     return response.status(200).json(surveyUser);
   }
