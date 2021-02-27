b0a610cf12f46fab67f2be6d239cb41b59b0cc6e
Daniel <danielsaraldi@gmail.com>
2021-02-26 23:52:42 -0300

    feat: ensure an survey user already exists

--- api/src/controllers/SendEmailController.ts
+++ api/src/controllers/SendEmailController.ts
@@ -28,21 +28,30 @@ class SendEmailController {
     if (!survey)
       return response.status(400).json({ error: "Survey does not exists!" });
 
-    const surveyUser = surveysUsersRepository.create({
-      user_id: user.id,
-      survey_id,
-    });
-    await surveysUsersRepository.save(surveyUser);
-
-    const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
-
     const variables = {
       name: user.name,
       title: survey.title,
       description: survey.description,
       user_id: user.id,
-      link: process.env.URL_MAIL
+      link: process.env.URL_MAIL,
     };
+
+    const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
+
+    const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+      where: [{ user_id: user.id }, { value: null }],
+    });
+
+    if (surveyUserAlreadyExists) {
+      await SendEmailService.execute(email, survey.title, variables, npsPath);
+      return response.status(200).json(surveyUserAlreadyExists);
+    }
+
+    const surveyUser = surveysUsersRepository.create({
+      user_id: user.id,
+      survey_id,
+    });
+    await surveysUsersRepository.save(surveyUser);
 
     await SendEmailService.execute(email, survey.title, variables, npsPath);
 
