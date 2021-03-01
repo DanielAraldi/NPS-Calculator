aabf6ba76b9f6bc430e7d58f5f2a4d228df69d1c
Daniel <danielsaraldi@gmail.com>
2021-03-01 20:24:09 -0300

    feat: create AnswerController controller

---
+++ api/src/controllers/AnswerController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
+
+class AnswerController {
+  async execute(request: Request, response: Response) {
+    const { value } = request.params;
+    const { survey } = request.query;
+
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+    const surveyUser = await surveysUsersRepository.findOne({
+      id: String(survey),
+    });
+
+    if (!surveyUser)
+      return response
+        .status(400)
+        .json({ error: "Survey User does not exists!" });
+
+    surveyUser.value = Number(value);
+
+    await surveysUsersRepository.save(surveyUser);
+
+    response.status(200).json(surveyUser);
+  }
+}
+
+export { AnswerController };
