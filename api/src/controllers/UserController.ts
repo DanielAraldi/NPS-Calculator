cc2af6bdc98428271dcd7c550fa82d161eebec08
Daniel <danielsaraldi@gmail.com>
2021-03-02 20:21:04 -0300

    feat: add validators user data

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,10 +1,25 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
+import * as yup from "yup";
 
 class UserController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required("Required name!"),
+      email: yup.string().email().required("Required e-mail!"),
+    });
+
+    // if (!(await schema.isValid(request.body)))
+    //   return response.status(400).json({ error: "Data schema failure!" });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
 
     const usersRepository = getCustomRepository(UsersRepository);
 
